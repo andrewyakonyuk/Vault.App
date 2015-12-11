1f727bc69ec54f08e5f46cbbe384e89959f6e814
Andrew Yakonyuk <andrewyakonyuk@gmail.com>
2015-12-11 17:29:11 +0200

    media viewport

---
+++ src/scripts/packages/mediaviewport.js
@@ -0,0 +1,9 @@
+/*global define */
+define(['jquery'], function($){
+  function Viewport(){
+    var $container = $('.media-container');
+    var $list = $container.find('.media-item');
+  }
+
+  return Viewport;
+});
--- src/scripts/pages/collection-page/collection.js
+++ src/scripts/pages/collection-page/collection.js
@@ -5,6 +5,8 @@ define(['knockout', 'text!./collection.html'], function (ko, template) {
 
     function CollectionViewModel(params) {
         console.log(params.id);
+        this.items = ko.observableArray([1,2,3,4,5,6,7,8,9,0]);
+        this.isViewportOpen = ko.observable(false);
     }
 
     CollectionViewModel.prototype = {
@@ -14,6 +16,15 @@ define(['knockout', 'text!./collection.html'], function (ko, template) {
 
         dispose: function () {
 
+        },
+
+        openViewport: function(){
+          console.log('openViewport')
+          this.isViewportOpen(true);
+        },
+
+        closeViewport: function(){
+          this.isViewportOpen(false);
         }
     };
 
