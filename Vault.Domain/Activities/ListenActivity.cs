ccfb0ab447dda8e2f049963deffe77865571a608
Andrew Yaconyc <andrewyaconyc@gmail.com>
2016-08-16 22:33:47 +0300

    remove unnecessary project

--- Vault.Domain/Activities/ListenActivity.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Vault.Domain.Activities
-{
-    /// <summary>
-    /// The act of consuming audio content.
-    /// </summary>
-    public class ListenActivity : ConsumeActivity
-    {
-    }
-}
